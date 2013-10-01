9a9d7683e47bf71705a445073f20335e11809f9b
Karsten Blees <blees@dcon.de>
2013-10-01 12:51:54 +0200

    mingw: add a cache below mingw's lstat and dirent implementations
    Checking the work tree status is quite slow on Windows, due to slow
    `lstat()` emulation (git calls `lstat()` once for each file in the
    index). Windows operating system APIs seem to be much better at scanning
    the status of entire directories than checking single files.
    
    Add an `lstat()` implementation that uses a cache for lstat data. Cache
    misses read the entire parent directory and add it to the cache.
    Subsequent `lstat()` calls for the same directory are served directly
    from the cache.
    
    Also implement `opendir()`/`readdir()`/`closedir()` so that they create
    and use directory listings in the cache.
    
    The cache doesn't track file system changes and doesn't plug into any
    modifying file APIs, so it has to be explicitly enabled for git functions
    that don't modify the working copy.
    
    Note: in an earlier version of this patch, the cache was always active and
    tracked file system changes via ReadDirectoryChangesW. However, this was
    much more complex and had negative impact on the performance of modifying
    git commands such as 'git checkout'.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/git-compat-util.h
+++ lib/git-compat-util.h
@@ -158,9 +158,11 @@ static inline int is_xplatform_dir_sep(int c)
 /* pull in Windows compatibility stuff */
 #include "compat/win32/path-utils.h"
 #include "compat/mingw.h"
+#include "compat/win32/fscache.h"
 #elif defined(_MSC_VER)
 #include "compat/win32/path-utils.h"
 #include "compat/msvc.h"
+#include "compat/win32/fscache.h"
 #endif
 
 /* used on Mac OS X */
@@ -1119,6 +1121,9 @@ static inline int is_missing_file_error(int errno_)
  */
 #ifndef enable_fscache
 #define enable_fscache(x) /* noop */
+#endif
+#ifndef flush_fscache
+#define flush_fscache() /* noop */
 #endif
 
 int cmd_main(int, const char **);
