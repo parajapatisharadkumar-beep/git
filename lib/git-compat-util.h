8f5830cb10d30d6b579f3420e2f10b8a288f3e7e
Ben Peart <benpeart@microsoft.com>
2018-11-02 11:19:10 -0400

    fscache: fscache takes an initial size
    Update enable_fscache() to take an optional initial size parameter which is
    used to initialize the hashmap so that it can avoid having to rehash as
    additional entries are added.
    
    Add a separate disable_fscache() macro to make the code clearer and easier
    to read.
    
    Signed-off-by: Ben Peart <benpeart@microsoft.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- lib/git-compat-util.h
+++ lib/git-compat-util.h
@@ -1126,6 +1126,10 @@ static inline int is_missing_file_error(int errno_)
 #define flush_fscache() /* noop */
 #endif
 
+#ifndef disable_fscache
+#define disable_fscache() /* noop */
+#endif
+
 #ifndef is_fscache_enabled
 #define is_fscache_enabled(path) (0)
 #endif
