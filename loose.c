434d24914dd6017b4e3f65822e31ae2bbd10dae9
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-12-19 12:50:18 +0100

    load_one_loose_object_map(): fix resource leak
    Pointed out by Coverity.
    
    While at it, reduce near-duplicate clean-up code at the end of the
    function.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- loose.c
+++ loose.c
@@ -65,6 +65,7 @@ static int load_one_loose_object_map(struct repository *repo, struct odb_source_
 {
 	struct strbuf buf = STRBUF_INIT, path = STRBUF_INIT;
 	FILE *fp;
+	int ret = -1;
 
 	if (!loose->map)
 		loose_object_map_init(&loose->map);
@@ -98,13 +99,12 @@ static int load_one_loose_object_map(struct repository *repo, struct odb_source_
 		insert_loose_map(loose, &oid, &compat_oid);
 	}
 
-	strbuf_release(&buf);
-	strbuf_release(&path);
-	return errno ? -1 : 0;
+	ret = 0;
 err:
+	fclose(fp);
 	strbuf_release(&buf);
 	strbuf_release(&path);
-	return -1;
+	return ret;
 }
 
 int repo_read_loose_object_map(struct repository *repo)
